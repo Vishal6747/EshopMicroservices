0de5606d5357dc23cb97dc8c9cfcd91984aa6c5f
vishal <dell@DESKTOP-VISHAL>
2024-10-26 23:57:58 +0530

    added CQRS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products
+{
+    public record CreateProductCommand(string Name, string Description, List<string> Category, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                Description = command.Description,
+                Category = command.Category,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
